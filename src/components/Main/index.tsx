7037000c1e1f181f73fe121fb9ad7c21c35a28c7
Josver_Marquez <josverlomejor@gmail.com>
2024-06-27 02:19:48 -0500

    prueba rutas 3

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import Styles from './main.module.css';
-//import { PublicRoutes } from '../../models/routes.model';
+import { PublicRoutes } from '../../models/routes.model';
 
 
 
@@ -13,7 +13,7 @@ const Main = () => {
         <h1>Las mejores peleas de jujutsu Kaisen</h1>
         <div className={Styles.contenido_column}>
           <p>En esta pagina se mostraran las que a mi parecer son de las mejores peleas de jujutsu kaisen, mencion honorifica a la pelea entre Vivy (Diva) y Yugo Kakitanai de Vivy -Fluorite Eye's Song.</p>
-          <button><Link to="/Vivy" className={Styles.link}>Ir a ver</Link></button>
+          <button><Link to={PublicRoutes.Vivy} className={Styles.link}>Ir a ver</Link></button>
         </div>
       </div>
     </section>
@@ -22,15 +22,15 @@ const Main = () => {
       <h2>New</h2>
       <ul>
         <li>
-          <Link to="/Gojo" className={Styles.link}>Gojo vs Sukuna</Link>
+          <Link to={PublicRoutes.Gojo} className={Styles.link}>Gojo vs Sukuna</Link>
           <p>La gran batalla entre el rey de las maldiciones, Ryomen Sukuna y el hechicero más poderoso de la historia, Satoru Gojo.</p>
         </li>
         <li>
-        <Link to="/Sukuna" className={Styles.link}>Sukuna vs Mahoraga</Link>
+        <Link to={PublicRoutes.Sukuna} className={Styles.link}>Sukuna vs Mahoraga</Link>
           <p>La gran batalla entre el shikigami mas poderoso de megumi fushiguro, Mahoraga y el rey de las maldiciones, Ryomen Sukuna.</p>
         </li>
         <li>
-        <Link to="Toji" className={Styles.link}>Toji vs Gojo</Link>
+        <Link to={PublicRoutes.Toji} className={Styles.link}>Toji vs Gojo</Link>
           <p>La gran ballata entre el que nacio con restricción celestial del clan zenin, Toji Fushiguro y el hechicero más poderoso de la historia, Satoru Gojo.</p>
         </li>
       </ul>
--- src/models/routes.model.ts
+++ src/models/routes.model.ts
@@ -1,12 +1,12 @@
-//export const PublicRoutes = {
-//  HOME: "/Home",
-//  Gojo: "/Gojo",
-//  Sukuna: "/Sukuna",
-//  Toji: "/Toji",
-//  Vivy: "/Vivy",
-//}
+export const PublicRoutes = {
+  HOME: "/Home",
+  Gojo: "/Gojo",
+  Sukuna: "/Sukuna",
+  Toji: "/Toji",
+  Vivy: "/Vivy",
+}
 
-//export const SubMenuRoutes = {
-//  Kaiju: "/Kaiju",
-//  Hells_Paradise: "/Hells_Paradise",
-//}
+export const SubMenuRoutes = {
+  Kaiju: "/Kaiju",
+  Hells_Paradise: "/Hells_Paradise",
+}
